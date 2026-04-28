24cf7603a6f5c5d976c9d1cc1d547d5f19265158
Xadeka <me@zade.dev>
2026-04-28 12:01:17 -0400

    Use util to apply classnames

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { useTimer } from "@/hooks/use-timer";
+import { cn } from "@/lib/utils";
 
 function App() {
   let [enabled, setEnabled] = React.useState(false);
   let timer = useTimer(30 * 60 * 1000, enabled);
-  let timerClasses = ["timer-anim"];
-  if (enabled) {
-    timerClasses.push("play");
-  }
   return (
     <>
       <button onClick={() => setEnabled(!enabled)}>Toggle Timer</button>
-      <div className={timerClasses.join(" ")} />
+      <div className={cn("timer-anim", enabled ? "play" : null)} />
       <p className="timer">{timer.format()}</p>
     </>
   );
