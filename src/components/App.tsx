f2112466066f8e6150a0fcb68a84e9b1c01500f8
Xadeka <me@zade.dev>
2026-04-28 12:42:28 -0400

    Update layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { useTimer } from "@/hooks/use-timer";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+type AnimationProps = {
+  enabled?: boolean;
+};
+
+function Animation(props: AnimationProps) {
+  return <div className={cn("timer-anim", props.enabled ? "play" : null)} />;
+}
 
 function App() {
   let [enabled, setEnabled] = React.useState(false);
   let timer = useTimer(30 * 60 * 1000, enabled);
   return (
-    <>
-      <button onClick={() => setEnabled(!enabled)}>Toggle Timer</button>
-      <div className={cn("timer-anim", enabled ? "play" : null)} />
-      <p className="timer">{timer.format()}</p>
-    </>
+    <div className="flex flex-col justify-evenly gap-2 p-2">
+      <Button
+        variant="default"
+        className="self-center"
+        onClick={() => setEnabled(!enabled)}
+      >
+        Toggle Timer
+      </Button>
+      <div className="flex flex-wrap-reverse place-items-center justify-center gap-x-20">
+        <Animation enabled={enabled} />
+        <p className="font-mono text-8xl font-extrabold">{timer.format()}</p>
+      </div>
+    </div>
   );
 }
 
